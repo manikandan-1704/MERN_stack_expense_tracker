b4623cb5155efd216a04faa0c21dcf3177416dd5
manikandan-1704 <manimk1704@gmail.com>
2025-03-27 12:21:08 +0530

    styled and handled login page layout

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,15 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AuthLayout from '../../components/layouts/AuthLayout'
+import { Link, useNavigate } from 'react-router-dom'
+import Input from '../../components/Inputs/Input'
+import { validateEmail } from '../../utils/helper'
 
 const Login = () => {
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+    const [error,setError] = useState(null)
+
+    const Navigate = useNavigate();
+
+    // Handle Login form Submit
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        if(!validateEmail(email)){
+            setError("Please Enter a valid email address")
+            return;
+        }
+        if(!password){
+            setError("Please Enter a password")
+            return;
+        }
+        setError("");
+
+        // Login API Call
+    }
     return(
-        <div>
+        <AuthLayout>
             <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center'>
                 <h3 className='text-xl font-semibold text-black'>Welcome Back</h3>
                 <p className='text-xs text-slate-700 mt-[5px] mb-6'>
                     Please enter your details to login
                 </p>
+            
+
+            <form onSubmit={handleLogin}>
+                <Input 
+                    value={email}
+                    onChange={({target})=>setEmail(target.value)}
+                    label='Email Address'
+                    placeholder='john@example.com'
+                    type="text"
+                    />
+                    <Input 
+                    value={password}
+                    onChange={({target})=>setPassword(target.value)}
+                    label='Passworrd'
+                    placeholder='Min 8 characters'
+                    type="password"
+                    />
+
+                    {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+
+                    <button type='submit' className='btn-primary'>
+                        LOGIN
+                    </button>
+
+                    <p className='text-[13px] text-slate-800 mt-3'>
+                        Don't have an account?{" "}
+                        <Link className='font-medium text-primary underline' to="/signUp">
+                            Sign Up
+                        </Link>
+                    </p>
+            </form>
             </div>
-        </div>
+        </AuthLayout>
     )
 }
 

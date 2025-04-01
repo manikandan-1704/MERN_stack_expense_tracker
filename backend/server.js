58ceff8aee03d5e8800d80ac062f45dc2b91cfef
manikandan-1704 <manimk1704@gmail.com>
2025-04-01 15:11:59 +0530

    added upload functionality

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,9 @@ connectDB();
 
 app.use("/api/v1/auth", authRoutes);
 
+//server uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);

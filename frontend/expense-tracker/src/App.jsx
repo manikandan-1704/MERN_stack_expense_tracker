b82bd78c1c829a79e163ca70aa3627507ee14a84
manikandan-1704 <manimk1704@gmail.com>
2025-04-02 14:25:13 +0530

    addded context

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -7,10 +7,12 @@ import SignUp from './pages/Auth/SignUp';
 import Home from './pages/Dashboard/Home';
 import Income from './pages/Dashboard/Income';
 import Expense from './pages/Dashboard/Expense';
+import UserProvider from './context/userContext';
 
 
 const App = () => {
   return(
+    <UserProvider>
     <div>
       <Router>
         <Routes>
@@ -23,6 +25,7 @@ const App = () => {
         </Routes>
       </Router>
     </div>
+    </UserProvider>
   )
 }
 
--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,16 +1,18 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import AuthLayout from '../../components/layouts/AuthLayout'
 import { Link, useNavigate } from 'react-router-dom'
 import Input from '../../components/Inputs/Input'
 import { validateEmail } from '../../utils/helper'
 import axiosInstance from '../../utils/axiosInstance'
 import { API_PATHS } from '../../utils/apiPath'
+import { UserContext } from '../../context/userContext'
 
 const Login = () => {
     const [email,setEmail] = useState('')
     const [password,setPassword] = useState('')
     const [error,setError] = useState(null)
 
+    const { updateUser } = useContext(UserContext);
     const navigate = useNavigate();
 
     // Handle Login form Submit
@@ -38,6 +40,7 @@ const Login = () => {
 
             if(token){
                 localStorage.setItem("token", token);
+                updateUser(user);
                 navigate("/dashboard");
             }
         }catch(error){

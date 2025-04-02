71af4433610650bf75017efc4cdb55b3fc04996e
manikandan-1704 <manimk1704@gmail.com>
2025-04-02 14:13:53 +0530

    integrated login api

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -3,13 +3,15 @@ import AuthLayout from '../../components/layouts/AuthLayout'
 import { Link, useNavigate } from 'react-router-dom'
 import Input from '../../components/Inputs/Input'
 import { validateEmail } from '../../utils/helper'
+import axiosInstance from '../../utils/axiosInstance'
+import { API_PATHS } from '../../utils/apiPath'
 
 const Login = () => {
     const [email,setEmail] = useState('')
     const [password,setPassword] = useState('')
     const [error,setError] = useState(null)
 
-    const Navigate = useNavigate();
+    const navigate = useNavigate();
 
     // Handle Login form Submit
     const handleLogin = async (e) => {
@@ -26,6 +28,25 @@ const Login = () => {
         setError("");
 
         // Login API Call
+
+        try{
+            const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+                email,
+                password
+            });
+            const { token, user } = response.data;
+
+            if(token){
+                localStorage.setItem("token", token);
+                navigate("/dashboard");
+            }
+        }catch(error){
+            if(error.response && error.response.data.message){
+                setError(error.response.data.message);
+            } else {
+                setError("Something went wrong. Please try again.");
+            }
+        }
     }
     return(
         <AuthLayout>
--- frontend/expense-tracker/src/utils/axiosInstance.js
+++ frontend/expense-tracker/src/utils/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { BASE_URL } from './apiPaths';
+import { BASE_URL } from './apiPath';
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,

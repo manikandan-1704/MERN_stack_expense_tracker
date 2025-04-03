f7eb0afc11b133a0f3ecdef52f1f5fefb38b17b7
manikandan-1704 <manimk1704@gmail.com>
2025-04-03 17:25:07 +0530

    modified home

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DashboardLayout from '../../components/layouts/DashboardLayout'
 import { userUserAuth } from '../../hooks/useUserAuth';
+import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPath';
 
 const Home = () => {
     userUserAuth();
+
+    const navigate = useNavigate();
+
+    const [dashboardData, setDashboardData] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const fetchDashboardData = async () => {
+        if(loading) return;
+
+        setLoading(true);
+
+        try{
+            const response = await axiosInstance.get(
+                `${API_PATHS.DASHBOARD.GET_DATA}`
+            );
+            if(response.data){
+                setDashboardData(response.data);
+            }
+        }catch(error){
+            console.log("Something Went Wrong. Please try again", error);
+        }finally{
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchDashboardData();
+        return () => {};
+    }, []);
+
     return (
         <DashboardLayout activeMenu="Dashboard">
             <div className='my-5 mx-auto'>

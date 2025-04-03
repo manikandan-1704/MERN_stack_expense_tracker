ec7ce7c7264fe5a7b0e33f683ed77c1916bb4804
manikandan-1704 <manimk1704@gmail.com>
2025-04-03 16:57:17 +0530

    added userauth

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import DashboardLayout from '../../components/layouts/DashboardLayout'
+import { userUserAuth } from '../../hooks/useUserAuth';
 
 const Home = () => {
-    useUserAuth();
+    userUserAuth();
     return (
         <DashboardLayout activeMenu="Dashboard">
             <div className='my-5 mx-auto'>

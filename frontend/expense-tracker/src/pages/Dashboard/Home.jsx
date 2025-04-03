1e0438a6ec16318db8543f024cdc32acd8c5ff88
manikandan-1704 <manimk1704@gmail.com>
2025-04-03 16:07:37 +0530

    designed side menu and added changes

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import DashboardLayout from '../../components/layouts/DashboardLayout'
 
 const Home = () => {
-    return(
+    useUserAuth();
+    return (
         <DashboardLayout activeMenu="Dashboard">
-            <div className='my-5 mx-auto'></div>
+            <div className='my-5 mx-auto'>
+                Home
+            </div>
         </DashboardLayout>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;

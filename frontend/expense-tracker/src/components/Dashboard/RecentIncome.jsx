ea84c1ac791820043f4277b60015f894ee33d3a2
manikandan-1704 <manimk1704@gmail.com>
2025-04-08 17:17:33 +0530

    completed recent income

--- frontend/expense-tracker/src/components/Dashboard/RecentIncome.jsx
+++ frontend/expense-tracker/src/components/Dashboard/RecentIncome.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { LuArrowRight } from 'react-icons/lu'
+import TransactionInfoCard from '../Cards/TransactionInfoCard'
+import moment from 'moment'
 
-const RecentIncome = () => {
+const RecentIncome = ({transactions, onSeeMore}) => {
     return (
-        <div>Recent Income</div>
+        <div className='card'>
+            <div className='flex items-center justify-between'>
+                <h5 className='text-lg'>Income</h5>
+
+                <button className='card-btn' onClick={onSeeMore}>
+                    See All <LuArrowRight className='text-base' />
+                </button>
+            </div>
+        <div className='mt-6'>
+            {transactions?.slice(0,5)?.map((item) => (
+                <TransactionInfoCard
+                    key={item._id}
+                    title={item.source}
+                    icon={item.icon}
+                    date={moment(item.date).format("DD MMM YYYY")}
+                    amount={item.amount}
+                    type="income"
+                    hideDeleteBtn
+                    />
+            ))}
+        </div>
+        </div>
     )
 }
 

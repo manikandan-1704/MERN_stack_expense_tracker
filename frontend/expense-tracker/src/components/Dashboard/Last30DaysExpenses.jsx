45f5fe5f59cf0921356c77ed362a7c2c43175ef8
manikandan-1704 <manimk1704@gmail.com>
2025-04-08 15:41:01 +0530

    added last 30 days expenses outline

---
+++ frontend/expense-tracker/src/components/Dashboard/Last30DaysExpenses.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Last30DaysExpenses = () => {
+    return (
+        <div>Last 30 Days Expenses</div>
+    )
+}
+
+export default Last30DaysExpenses;
--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -12,6 +12,7 @@ import { addThousandsSeparator } from '../../utils/helper';
 import RecentTransactions from '../../components/Dashboard/RecentTransactions';
 import FinanceOverview from '../../components/Dashboard/FinanceOverview';
 import ExpenseTransactions from '../../components/Dashboard/ExpenseTransactions';
+import Last30DaysExpenses from '../../components/Dashboard/last30DaysExpenses';
 const Home = () => {
     userUserAuth();
 
@@ -82,6 +83,10 @@ const Home = () => {
                 <ExpenseTransactions
                     transactions = {dashboardData?.last30DaysExpenses?.transactions || []}
                     onSeeMore={() => navigate("/expense")}
+                />
+
+                <Last30DaysExpenses
+                    data={dashboardData?.last30DaysExpenses?.transactions || []}
                 />
                 </div>
             </div>

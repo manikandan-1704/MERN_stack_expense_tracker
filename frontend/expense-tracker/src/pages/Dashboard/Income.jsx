57d18ba14d81e177fe060aa7dbd995ebaa8257c1
manikandan-1704 <manimk1704@gmail.com>
2025-04-08 17:26:22 +0530

    modified income layput

--- frontend/expense-tracker/src/pages/Dashboard/Income.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Income.jsx
@@ -5,7 +5,14 @@ const Income = () => {
     return (
         <DashboardLayout activeMenu="Income">
             <div className='my-5 mx-auto'>
-
+                <div className='grid grid-cols-1 gap-6'>
+                    <div className=''>
+                        <IncomeOverview
+                            transactions={incomeData}
+                            onAddIncome={() => setOpenAddIncomeModal(true)}
+                        />
+                    </div>
+                </div>
             </div>
         </DashboardLayout>
     )

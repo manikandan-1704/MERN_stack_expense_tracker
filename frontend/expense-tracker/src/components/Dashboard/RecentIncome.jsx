078cb661f51676859424ac3a59cf4325e31c8816
manikandan-1704 <manimk1704@gmail.com>
2025-04-08 17:07:29 +0530

    added recent income outline

---
+++ frontend/expense-tracker/src/components/Dashboard/RecentIncome.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RecentIncome = () => {
+    return (
+        <div>Recent Income</div>
+    )
+}
+
+export default RecentIncome
--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -14,6 +14,7 @@ import FinanceOverview from '../../components/Dashboard/FinanceOverview';
 import ExpenseTransactions from '../../components/Dashboard/ExpenseTransactions';
 import Last30DaysExpenses from '../../components/Dashboard/last30DaysExpenses';
 import RecentIncomeWithChart from '../../components/Dashboard/RecentIncomeWithChart';
+import RecentIncome from '../../components/Dashboard/RecentIncome';
 const Home = () => {
     userUserAuth();
 
@@ -93,6 +94,11 @@ const Home = () => {
                 <RecentIncomeWithChart
                     data={dashboardData?.last60DaysIncome?.transactions?.slice(0,4) || []}
                     totalIncome={dashboardData?.totalIncome || 0}
+                    />
+
+                <RecentIncome
+                    transactions = {dashboardData?.last60DaysIncome?.transactions || []}
+                    onSeeMore={() => navigate("/income")}
                     />
                 </div>
             </div>

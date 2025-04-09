899cb4592dbfb90d17588a84fdbe6b9cce58ba4f
manikandan-1704 <manimk1704@gmail.com>
2025-04-09 10:49:53 +0530

    added helper functionality

--- frontend/expense-tracker/src/components/Income/IncomeOverview.jsx
+++ frontend/expense-tracker/src/components/Income/IncomeOverview.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { LuPlus } from 'react-icons/lu';
+import CustomBarChart from '../Charts/CustomBarChart';
+import { prepareIncomeBarChartData } from '../../utils/helper';
 
 const IncomeOverview = ({ transactions, onAddIncome }) => {
+    const [chartData, setChartData] = useState([]);
+    useEffect(() => {
+        const result = prepareIncomeBarChartData(transactions);
+        setChartData(result);
+
+        return () => {};
+    },[transactions]);
     return (
         <div>IncomeOverview</div>
     )

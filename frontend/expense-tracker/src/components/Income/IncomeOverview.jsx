a3626772c4a6fd2b0532121ab9f303f1960815c2
manikandan-1704 <manimk1704@gmail.com>
2025-04-09 11:00:23 +0530

    added add income card layout

--- frontend/expense-tracker/src/components/Income/IncomeOverview.jsx
+++ frontend/expense-tracker/src/components/Income/IncomeOverview.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { LuPlus } from 'react-icons/lu';
 import CustomBarChart from '../Charts/CustomBarChart';
 import { prepareIncomeBarChartData } from '../../utils/helper';
@@ -12,7 +12,23 @@ const IncomeOverview = ({ transactions, onAddIncome }) => {
         return () => {};
     },[transactions]);
     return (
-        <div>IncomeOverview</div>
+        <div className='card'>
+            <div className='flex items-center justify-between'>
+                <div className=''>
+                    <h5 className='text-lg'>Income Overview</h5>
+                    <p className='text-xs text-gray-400 mt-0.5'>
+                        Track your earnings overtime and analyze your income trends.
+                    </p>
+                </div>
+                <button className='add-btn' onClick={onAddIncome}>
+                    <LuPlus className='text-lg' />
+                    Add Income
+                </button>
+            </div>
+            <div className='mt-10'>
+
+            </div>
+        </div>
     )
 }
 

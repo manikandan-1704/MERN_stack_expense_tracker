5f351e3f23b3358349f2cb413fb4f19abb340aba
manikandan-1704 <manimk1704@gmail.com>
2025-04-09 12:33:48 +0530

    added icon

--- frontend/expense-tracker/src/components/Income/AddIncomeForm.jsx
+++ frontend/expense-tracker/src/components/Income/AddIncomeForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Input from '../Inputs/Input'
+import EmojiPickerPopup from '../EmojiPickerPopup';
 
 const AddIncomeForm = ({onAddIncome}) => {
     const [income, setIncome] = useState({

30b1ef1a3f7d82dabbc603212052fb1341f92365
manikandan-1704 <manimk1704@gmail.com>
2025-04-09 10:25:43 +0530

    added income overview layout

---
+++ frontend/expense-tracker/src/components/Income/IncomeOverview.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IncomeOverview = () => {
+    return (
+        <div>IncomeOverview</div>
+    )
+}
+
+export default IncomeOverview
--- frontend/expense-tracker/src/pages/Dashboard/Income.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Income.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DashboardLayout from '../../components/layouts/DashboardLayout'
 
 const Income = () => {
+    const [openAddIncomeModal, setOpenAddIncomeModal] = useState(false);
     return (
         <DashboardLayout activeMenu="Income">
             <div className='my-5 mx-auto'>

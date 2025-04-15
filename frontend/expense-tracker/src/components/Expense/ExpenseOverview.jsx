16f1a32a2e0a65d8aef470a38128cba12fa64897
manikandan-1704 <manimk1704@gmail.com>
2025-04-15 13:59:07 +0530

    modified expense overview

--- frontend/expense-tracker/src/components/Expense/ExpenseOverview.jsx
+++ frontend/expense-tracker/src/components/Expense/ExpenseOverview.jsx
@@ -2,17 +2,31 @@ import React, { useEffect, useState } from 'react'
 import { LuPlus } from 'react-icons/lu';
 import { prepareExpenseLineChartData } from '../../utils/helper';
 
-const ExpenseOverview = ({transactions, onExpenseIncome}) => {
+const ExpenseOverview = ({ transactions, onExpenseIncome }) => {
     const [chartData, setChartData] = useState([]);
 
     useEffect(() => {
         const result = prepareExpenseLineChartData(transactions);
         setChartData(result);
 
-        return () => {};
+        return () => { };
     }, [transactions]);
     return (
-        <div>ExpenseOverview</div>
+        <div className='card'>
+            <div className='flex items-center justify-between '>
+                <div className=''>
+                    <h5 className='text-lg'>Expense Overview</h5>
+                    <p className='text-xs text-gray-400 mt-0.5'>Track your expenses overtime and analyze your expense trends.</p>
+                </div>
+                <button className='add-btn' onClick={onExpenseIncome}>
+                    <LuPlus className='text-lg' />
+                    Add Expense
+                </button>
+            </div>
+            <div className='mt-10'>
+                <CustomLineChart data={chartData} />
+            </div>
+        </div>
     )
 }
 

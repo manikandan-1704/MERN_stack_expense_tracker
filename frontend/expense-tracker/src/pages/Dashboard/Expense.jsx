c17cb00f148125ab00d83133400340c2bd9057d9
manikandan-1704 <manimk1704@gmail.com>
2025-04-15 12:40:57 +0530

    added layout for expense

--- frontend/expense-tracker/src/pages/Dashboard/Expense.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Expense.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { userUserAuth } from '../../hooks/useUserAuth';
+import DashboardLayout from '../../components/layouts/DashboardLayout';
 
 const Expense = () => {
+    userUserAuth();
+        const [expenseData, setExpenseData] = useState([]);
+        const [loading, setLoading] = useState(false);
+        const [openDeleteAlert, setOpenDeleteAlert] = useState({
+            show: false,
+            data: null,
+        });
+    
+        const [openAddExpenseModal, setOpenAddExpenseModal] = useState(false);
     return (
-        <div>Expense</div>
+        <DashboardLayout activeMenu="Expense">
+            <div className='my-5 mx-auto'>
+        </div>
+        </DashboardLayout>
     )
 }
 

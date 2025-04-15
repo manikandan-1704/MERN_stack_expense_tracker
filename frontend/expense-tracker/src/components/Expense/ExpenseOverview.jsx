6d9b1f066be521421b307b48401798b01d91ceb5
manikandan-1704 <manimk1704@gmail.com>
2025-04-15 13:53:15 +0530

    added helper function

--- frontend/expense-tracker/src/components/Expense/ExpenseOverview.jsx
+++ frontend/expense-tracker/src/components/Expense/ExpenseOverview.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { LuPlus } from 'react-icons/lu';
+import { prepareExpenseLineChartData } from '../../utils/helper';
 
-const ExpenseOverview = () => {
+const ExpenseOverview = ({transactions, onExpenseIncome}) => {
+    const [chartData, setChartData] = useState([]);
+
+    useEffect(() => {
+        const result = prepareExpenseLineChartData(transactions);
+        setChartData(result);
+
+        return () => {};
+    }, [transactions]);
     return (
         <div>ExpenseOverview</div>
     )

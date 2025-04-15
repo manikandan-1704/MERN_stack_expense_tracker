cf1a18ce69b8ed59f132f1b185027fb7a1d2f7f4
manikandan-1704 <manimk1704@gmail.com>
2025-04-15 15:12:54 +0530

    added downloading expense details

--- frontend/expense-tracker/src/pages/Dashboard/Expense.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Expense.jsx
@@ -95,7 +95,31 @@ const Expense = () => {
     };
 
     // Handle Download expense details
-    const handleDownloadExpenseDetails = async () => {};
+    const handleDownloadExpenseDetails = async () => {
+        try{
+            const response = await axiosInstance.get(
+                API_PATHS.EXPENSE.DOWNLOAD_EXPENSE,
+                {
+                    responseType: "blob",
+                }
+            );
+
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+            const link = document.createElement("a");
+            link.href = url;
+            link.setAttribute("download", "expense_details.xlsx");
+            document.body.appendChild(link);
+            link.click();
+            link.parentNode.removeChild(link);
+            window.URL.revokeObjectURL(url);
+
+        }catch(error){
+            console.error(
+                "Error downloading expense details", error
+            );
+            toast.error("Failed to download expense details");
+        }
+    };
     useEffect(() => {
         fetchExpenseDetails();
 
